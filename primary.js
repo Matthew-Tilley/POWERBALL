d90ef1d63f94155df7665f9a47bbd9d995882ffc
Matt Tilley <mtilley255@gmail.com>
2017-08-23 09:29:04 -0500

    Added Math.floor & tested.
    Appears to be working properly.

--- primary.js
+++ primary.js
@@ -2,7 +2,7 @@ $(document).ready(function()
 {
 	$("#BTN01").on("click", function()
 	{
-		$("#PBN01").val(Math.random());
+		$("#PBN01").val(Math.floor(Math.random()));
 		
 		$/*("#PBN01").val("450");*/
 	});
