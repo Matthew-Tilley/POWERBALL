5f5c29fb7e62e807fc43c73bc127ea0ae9e86983
Matt Tilley <mtilley255@gmail.com>
2017-08-23 09:45:01 -0500

    Added some variables.

--- primary.js
+++ primary.js
@@ -2,8 +2,14 @@ $(document).ready(function()
 {
 	$("#BTN01").on("click", function()
 	{
-	
-		$("#PBN01").val(Math.ceil(Math.random()* 69));
+		var B1 = document.getElementById("PBN01")
+		var B2 = document.getElementById("PBN02")
+		
+		var all = [B1, B2];
+		
+		
+		
+		/*$("#PBN01").val(Math.ceil(Math.random()* 69));*/
 		
 		$/*("#PBN01").val("450");*/
 	});
