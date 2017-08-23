e4bd6c81562cc618d307f78a0c29c9706cf8dc85
Matt Tilley <mtilley255@gmail.com>
2017-08-23 09:41:37 -0500

    Made math correction / changed code.

--- primary.js
+++ primary.js
@@ -2,7 +2,8 @@ $(document).ready(function()
 {
 	$("#BTN01").on("click", function()
 	{
-		$("#PBN01").val(Math.ceil(Math.random()));
+	
+		$("#PBN01").val(Math.ceil(Math.random()* 69));
 		
 		$/*("#PBN01").val("450");*/
 	});
