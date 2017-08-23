2ab8513152c1957e24e28a2589193e69843fe407
Matt Tilley <mtilley255@gmail.com>
2017-08-23 14:52:24 -0500

    IT WORKS!!!
    Still need to DRY code at some point, but the basic concept is there...and functional.

--- primary.js
+++ primary.js
@@ -2,15 +2,18 @@ $(document).ready(function()
 {
 	$("#BTN01").on("click", function()
 	{
-		var B1 = document.getElementById("WB01")
-		var B2 = document.getElementById("WB02")
+		var WB01 = document.getElementById("WB01")
+		var WB02 = document.getElementById("WB02")
+		var WB03 = document.getElementById("WB03")
+		var WB04 = document.getElementById("WB04")
+		var WB05 = document.getElementById("WB05")
+		var PB00 = document.getElementById("PB00")
 		
-		var allof = [B1, B2];
-		
-		console.log($(allof).val());
-		
-		/*$("#PBN01").val(Math.ceil(Math.random()* 69));*/
-		
-		$/*("#PBN01").val("450");*/
+		$(WB01).val((Math.ceil(Math.random() * 69 )));
+		$(WB02).val((Math.ceil(Math.random() * 69 )));
+		$(WB03).val((Math.ceil(Math.random() * 69 )));
+		$(WB04).val((Math.ceil(Math.random() * 69 )));
+		$(WB05).val((Math.ceil(Math.random() * 69 )));
+		$(PB00).val((Math.ceil(Math.random() * 26 )));
 	});
 });
