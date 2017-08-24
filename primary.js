77ac394eaeefe4af2906181c795a3c357a151771
Matt Tilley <mtilley255@gmail.com>
2017-08-24 09:18:22 -0500

    Simple test code
    I know my math was correct, but randomly thought of a way to kinda test it.  Coded it up right quick...just because.

--- primary.js
+++ primary.js
@@ -1,10 +1,22 @@
 $(document).ready(function()
 {
 	
-	var random_white = Math.ceil( Math.random() * 69 );
+	for (i=0; i<50000; i++)
+	{
+		var random_white = Math.ceil( Math.random() * 69 );
+		
+		if (random_white < 70)
+		{
+			console.log(random_white + " Yay!")
+		}
+		
+		else
+		{
+			console.log("PROBLEM");
+		}
 	
-	console.log(random_white);
-	
+		
+	}
 	
 	
 	
